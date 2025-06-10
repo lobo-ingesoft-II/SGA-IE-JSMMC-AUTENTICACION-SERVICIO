b6d6abcc0b001607c7bd6b9c449ccc6f346c4bd1
Diego-Alvarez19 <diegolds221@gmail.com>
2025-06-10 11:56:40 -0500

    ocultacion de credenciales

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://uwyl6ydk2wi90jzi:hGdw1dBMo0Fxg78F6Zav@bbfzaofbtbqcke4vwvkh-mysql.services.clever-cloud.com:3306/bbfzaofbtbqcke4vwvkh" # PONER LAS CREDENCIALES QUE COMPARTIO JHOAN 
+DATABASE_URL = "" # PONER LAS CREDENCIALES QUE COMPARTIO JHOAN 
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
