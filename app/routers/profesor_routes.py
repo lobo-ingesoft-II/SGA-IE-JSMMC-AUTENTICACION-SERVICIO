5c84445772cb23ab581bb3129bb2529c4516cd4d
JhoanSFranco <jhfrancor@unal.edu.co>
2025-07-01 11:05:38 -0500

    Cambios ruta profesor

--- app/routers/profesor_routes.py
+++ app/routers/profesor_routes.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends
 from app.services.auth_service import role_required
+from app.backend.database import get_db
+from app.models.profesor_model import Profesor
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix="/profesor",
@@ -10,3 +13,11 @@ router = APIRouter(
 @router.get("/")
 def vista_profesor():
     return {"message": "Bienvenido, Profesor"}
+
+@router.get("/{id_profesor}")
+def get_profesor(id_profesor: int, db: Session = Depends(get_db)):
+    dic_curso = db.query(Profesor).filter(Profesor.id_profesor == id_profesor).first()
+    if dic_curso:
+        return dic_curso
+    else:
+        return {"message": "Profesor no encontrado"}, 404
