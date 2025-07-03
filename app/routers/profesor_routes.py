45df68ba6d9914c54af1b962746f794a48d0454d
JhoanSFranco <jhfrancor@unal.edu.co>
2025-07-02 20:49:22 -0500

    cambio ruta Profesor

--- app/routers/profesor_routes.py
+++ app/routers/profesor_routes.py
@@ -3,6 +3,7 @@ from app.services.auth_service import role_required
 from app.backend.database import get_db
 from app.models.profesor_model import Profesor
 from sqlalchemy.orm import Session
+from http import HTTPException
 
 router = APIRouter(
     prefix="/profesor",
@@ -14,10 +15,42 @@ router = APIRouter(
 def vista_profesor():
     return {"message": "Bienvenido, Profesor"}
 
+
 @router.get("/{id_profesor}")
 def get_profesor(id_profesor: int, db: Session = Depends(get_db)):
-    dic_curso = db.query(Profesor).filter(Profesor.id_profesor == id_profesor).first()
-    if dic_curso:
-        return dic_curso
-    else:
-        return {"message": "Profesor no encontrado"}, 404
+    profesor = db.query(Profesor).filter(Profesor.id_profesor == id_profesor).first()
+    if not profesor:
+        raise HTTPException(status_code=404, detail="Profesor no encontrado")
+    return {"id_profesor": profesor.id_profesor, "id_usuario": profesor.id_usuario, "especialidad": profesor.especialidad, "es_director": profesor.es_director}
+    
+
+# # Obtener usuario por ID
+# @router.get("/profesores/{id_profesor}")
+# def get_profesor(id_profesor: int, db: Session = Depends(get_db)):
+#     # Buscar el profesor por ID
+#     profesor = db.query(Profesor).filter(Profesor.id_profesor == id_profesor).first()
+#     if not profesor:
+#         raise HTTPException(status_code=404, detail="Profesor no encontrado")
+    
+#     # Buscar el usuario asociado manualmente
+#     usuario = db.query(UsuarioModel).filter(UsuarioModel.id_usuario == profesor.id_usuario).first()
+#     if not usuario:
+#         raise HTTPException(status_code=404, detail="Usuario asociado no encontrado")
+
+#     # Combinar los datos de profesor y usuario en un solo dict
+#     return {
+#         "id_profesor": profesor.id_profesor,
+#         "id_usuario": profesor.id_usuario,
+#         "especialidad": profesor.especialidad,
+#         "es_director": profesor.es_director,
+#         "nombres": usuario.nombres,
+#         "apellidos": usuario.apellidos,
+#         "tipo_documento": usuario.tipo_documento,
+#         "documento_identidad": usuario.documento_identidad,
+#         "telefono": usuario.telefono,
+#         "email": usuario.email,
+#         "rol": usuario.rol,
+#         "estado": usuario.estado,
+#         "fecha_creacion": usuario.fecha_creacion,
+#         "fecha_modificacion": usuario.fecha_modificacion
+#     }
