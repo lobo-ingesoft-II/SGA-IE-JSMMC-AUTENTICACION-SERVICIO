4c2109037d06ac0e8b042c1e22476585a59bd6da
Diego-Alvarez19 <diegolds221@gmail.com>
2025-07-16 21:24:14 -0500

    Se agrego la parte de observabilidad con prometheus

--- app/main.py
+++ app/main.py
@@ -6,6 +6,12 @@ from app.routers import auth_routes, admin_routes, profesor_routes, acudiente_ro
 
 from app.backend.session import Base, engine
 
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from starlette.responses import Response
+from app.observability.metrics import REQUEST_COUNT, REQUEST_LATENCY, ERROR_COUNT
+import time
+from fastapi import Request
+
 #Lee todas las clases que heredan de Base.
 # Genera el SQL necesario para crear las tablas en la base de datos.
 # No borra ni modifica tablas existentes, solo crea las que faltan.
@@ -21,7 +27,30 @@ app.include_router(admin_routes.router)
 app.include_router(profesor_routes.router)
 app.include_router(acudiente_routes.router)
 
+@app.middleware("http")
+async def metrics_middleware(request: Request, call_next):
+    start_time = time.time()
+    try:
+        response = await call_next(request)
+        status = response.status_code
+    except Exception:
+        status = 500
+        raise
+    finally:
+        latency = time.time() - start_time
+        method = request.method
+        endpoint = request.url.path
 
+        REQUEST_COUNT.labels(method=method, endpoint=endpoint).inc()
+        REQUEST_LATENCY.labels(method=method, endpoint=endpoint).observe(latency)
+        if status >= 400:
+            ERROR_COUNT.labels(method=method, endpoint=endpoint, status_code=str(status)).inc()
+
+    return response
+
+@app.get("/metrics")
+def metrics():
+    return Response(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 # Configuración de CORS
 app.add_middleware(
