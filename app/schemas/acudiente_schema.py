8b144fa8ef8808bb22d73c8cf3b244557dc5da41
JhoanSFranco <jhfrancor@unal.edu.co>
2025-07-20 10:32:37 -0500

    Update acudiente_schema.py

--- app/schemas/acudiente_schema.py
+++ app/schemas/acudiente_schema.py
@@ -1,15 +1,14 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional, Literal
-from datetime import date, datetime
+from pydantic import BaseModel
+from typing import Optional
 
-class AcudienteSchema(BaseModel):
-    nombre: str
-    apellido: str
-    documento: str
-    tipo_documento: Literal["CC", "TI", "CE", "PA"]
-    fecha_nacimiento: date
-    telefono: Optional[str]
-    email: Optional[EmailStr]
-    direccion: Optional[str]
-    parentesco: Literal["Padre", "Madre", "Tío", "Tía", "Abuelo", "Abuela", "Otro"]
-    fecha_registro: Optional[datetime]
+class AcudienteBase(BaseModel):
+    id_usuario: int
+    parentesco: Optional[str] = None
+    celular: Optional[str] = None
+    direccion: Optional[str] = None
+
+class AcudienteOut(AcudienteBase):
+    id_acudiente: int
+
+    class Config:
+        orm_mode = True
